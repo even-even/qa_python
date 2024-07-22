a10f69d986f099ba6c55fb5c271b3ab167c4e10e
even-even <vladimir_roschin@rambler.ru>
2024-07-22 17:26:05 +0300

    resolve enumerate and up pytest_lib version

--- sprint_4/conftest.py
+++ sprint_4/conftest.py
@@ -20,10 +20,9 @@ def collection_five_books(collection):
     collect = collection
     books = ['Властелин колец', 'Король лев', 'Чужой', 'Сон в летнюю ночь', 'Молчание ягнят']
     genre = ['Фантастика', 'Мультфильмы', 'Ужасы', 'Комедии', 'Детективы']
-    for i in range(5):
-        collect.add_new_book(books[i])
 
-    for i in range(5):
-        collect.set_book_genre(books[i], genre[i])
+    for i, book in enumerate(books):
+        collect.add_new_book(book)
+        collect.set_book_genre(book, genre[i])
 
     return collect
